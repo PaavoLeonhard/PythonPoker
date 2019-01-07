16a7504a01a4994313330ae36eda24634589934b
veit98 <veit.schumacher@gmail.com>
2019-01-07 11:44:43 +0100

    Adds: - draw letters on cards (King, Queen, Boy) - draw card borders on the table for the poker cards in the middle

--- assets/js/gui.js
+++ assets/js/gui.js
@@ -41,27 +41,139 @@ const CardText = {
   11: "B"
 };
 
+const EasingFunction = {
+  easeIn: t => {return t*t*t},
+  easeOut: t => {return (--t)*t*t+1},
+  easeInOut: t => {return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1},
+  linear: t => {return t}
+}
+
 // RENDERING STUFF HERE
+
+class MovingObject {
+  constructor(x,y,rotation) {
+    // OBJECTS REAL PROPERTIES
+    this.x = x;
+    this.y = y;
+    this.rotation = rotation;
+    // ANIMATION PROPERTIES
+    this.eF = EasingFunction.linear;
+    this.startTime = 0;
+    this.lastTime = 0;
+    this.animTime = 0;
+    this.animLen = 0;
+    this.direction = {x:0,y:0};
+    this.startPos = {x:0,y:0};
+    this.endPos = {x:x,y:y};
+    this.startRotation = 0;
+    this.rotationLen = 0;
+    this.delay = 0;
+    this.init = false;
+    this.running = false;
+  }
+  //time in milliseconds
+  moveTo(pos,rotation,easing,time,delay) {
+    this.startTime = 0;
+    this.init = true;
+    this.animTime = time;
+    this.startPos.x = this.x;
+    this.startPos.y = this.y;
+    this.endPos.x = pos.x;
+    this.endPos.y = pos.y;
+    let dx = this.direction.x,
+    dy = this.direction.y;
+    dx = pos.x-this.x;
+    dy = pos.y-this.y;
+    this.animLen = Math.sqrt(Math.pow(dx,2)+Math.pow(dy,2));
+    this.direction.x = 1 / this.animLen * dx;
+    this.direction.y = 1 / this.animLen * dy;
+
+    this.startRotation = this.rotation;
+    this.rotationLen = rotation;
+
+    this.delay = delay;
+
+    switch(easing) {
+      case 'ease-in': this.eF = EasingFunction.easeIn;break;
+      case 'ease-out': this.eF = EasingFunction.easeOut;break;
+      case 'ease-in-out': this.eF = EasingFunction.easeInOut;break;
+      default: this.eF = EasingFunction.linear;
+    }
+  }
+
+  update() {
+    if(this.delay > 0) {
+      let delta, now;
+      if(this.lastTime === 0) {
+        delta = 10;
+      } else {
+        now = Date.now();
+        delta = now - this.lastTime;
+        this.lastTime = now;
+      }
+      this.delay -= delta;
+      if(this.delay < 0) this.delay = 0;
+    }
+    if(this.init && this.delay === 0) {
+      this.startTime = Date.now();
+      this.init = !this.init;
+      this.running = true;
+    }
+    if(this.running) {
+      let now = Date.now();
+      let tia = now - this.startTime;
+      let t = tia / this.animTime;
+      t = t > 1 ? 1 : t;
+      let fac = this.eF(t);
+
+      this.x = this.startPos.x + this.direction.x * fac * this.animLen;
+      this.y = this.startPos.y + this.direction.y * fac * this.animLen;
+      this.rotation = this.startRotation + fac * this.rotationLen;
+
+      if(this.endPos.x === this.x && this.endPos.y === this.y) {
+        this.running = !this.running;
+      }
+    }
+  }
+}
+
 class PokerGameGui {
   // maybe as player json?
-  constructor(players) {
+  constructor(players,props) {
+
     this.props = {
       canvas: {
-        width: undefined, //IF NOT SET IT WILL BE THE WHOLE WINDOW
-        height: undefined,
-        id: 'canvas'
-      },
-    };
+        id: props ? props.canvas.id : 'poker-canvas' ,
+        width: props ? props.canvas.width : undefined ,
+        height: props ? props.canvas.height : undefined
+      }
+    }
 
     this.players = players;
+    this.cards = [];
+    this.chips = [];
     this.setupCanvas();
+    this.scale = this.canvas.height * .0017 * 50;
     this.table = new Table(this.ctx);
     window.addEventListener("resize", () => {
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
-        this.reset();
+        this.scale = this.canvas.height * .0017 * 50;
+        this.start();
     });
     //this.start();
+  }
+
+  addCard(type,value) {
+    let card = new Card(this.canvas.width*.5, this.canvas.height*.5,0,type,value,this.ctx);
+    card.moveTo({x:300,y:300},180,'ease-out',1000,2000);
+    card.flip();
+    this.cards.push(card);
+  }
+
+  addChip(value) {
+    let chip = new Chip(200,200,0,value,Color.red,this.ctx);
+    this.chips.push(chip);
   }
 
   clear() {
@@ -72,24 +184,28 @@ class PokerGameGui {
   reset() {
     this.clear();
     this.table.draw();
-    let card1 = new Card(this.canvas.width*.5, this.canvas.height*.5,this.canvas.height * .0013 * 50,45,CardType.PIKES,CardValue.TEN,true,this.ctx);
-    let card2 = new Card(this.canvas.width*.5 -200, this.canvas.height*.5,this.canvas.height * .0013 * 50,-45,CardType.HEARTS,CardValue.FIVE,true,this.ctx);
-    let chip1 = new Chip(500,400,10,13,10,'#ce5f5f',this.ctx);
-    card1.draw();
-    card2.draw();
-    chip1.draw();
-
-    this.raf = requestAnimationFrame(() => this.reset());
 
   }
 
   renderIngame() {
+    this.reset();
+    this.renderGO();
+  }
 
+  renderGO() {
+    for(let card of this.cards) {card.draw()};
+    for(let chip of this.chips) {chip.draw()};
+  }
+
+  updateGO() {
+    for(let card of this.cards) {card.update();}
+    for(let chip of this.chips) {chip.update();}
   }
 
   start() {
-    this.raf = requestAnimationFrame(() => this.start());
+    this.updateGO();
     this.renderIngame();
+    this.raf = requestAnimationFrame(() => this.start());
   }
 
   stop() {
@@ -108,20 +224,15 @@ class PokerGameGui {
     document.body.appendChild(this.canvas);
     this.ctx = this.canvas.getContext('2d');
   }
-
-
-
 }
 
-class Card {
-  constructor(x, y, width,rotation, type, value, face, ctx) {
+class Card extends MovingObject{
+  constructor(x, y,rotation, type, value, ctx) {
+    super(x,y,rotation);
     // RENDER SPECIFIC
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.rotation = rotation;
+    //this.width = width;
     this.ctx = ctx;
-    this.face = face;
+    this.face = false;
     // CARD SPECIFIC
     this.type = type;
     this.value = value;
@@ -131,9 +242,12 @@ class Card {
     if(this.face) {
       this.drawFace();
     } else {
-      // this.drawBack();
       this.drawBack();
     }
+  }
+
+  flip() {
+    this.face = !this.face;
   }
 
   /* Older implementation of Card backside
@@ -212,8 +326,8 @@ class Card {
 
   drawBack() {
     let ctx = this.ctx,
-    scale = ctx.canvas.height * .0013,
-    w = this.width,
+    //w = this.width,
+    w = ctx.canvas.height * .085,
     h = 1.3*w,
     r = .06*w,
     x = this.x,
@@ -222,6 +336,8 @@ class Card {
     // CARD SHAPE
 
     // GLOBAL CARD ROTATION
+    ctx.save();
+
     ctx.translate(x, y);
     ctx.rotate(this.rotation * Math.PI / 180);
     ctx.translate(-x, -y);
@@ -256,19 +372,14 @@ class Card {
       ctx.fillRect(x-w*.8+(w*.07*i),y-h*.8,w*.02,h*1.6);
     }
     ctx.restore();
-
-    //GLOBAL CARD ROTATION RESET
-    ctx.translate(x, y)
-    ctx.rotate(-this.rotation * Math.PI / 180);
-    ctx.translate(-x, -y)
-
-
+    // RESTORE GLOBAL CARD ROTATION
+    ctx.restore();
   }
 
   drawFace() {
     let ctx = this.ctx,
-    //scale = ctx.canvas.height * .0013,
-    w = this.width,
+    // w = this.width,
+    w = ctx.canvas.height * .085,
     h = 1.3*w,
     r = .06*w,
     x = this.x,
@@ -297,9 +408,9 @@ class Card {
       case 14:
         CardSymbol.draw({x: x, y: y},scale*2,0,this.type,ctx);
         break;
-      case 13: if (this.value === 13) CardLetter.draw({x: x, y: y+h*.15},scale*3.5,this.type,13,ctx);
-      case 12: if (this.value === 12) CardLetter.draw({x: x, y: y+h*.15},scale*3.5,this.type,12,ctx);
-      case 11: if (this.value === 11) CardLetter.draw({x: x, y: y+h*.15},scale*3.5,this.type,11,ctx);
+      case 13: if (this.value === 13) CardLetter.draw({x: x, y: y+h*.17},scale*3.5,this.type,13,ctx);
+      case 12: if (this.value === 12) CardLetter.draw({x: x, y: y+h*.17},scale*3.5,this.type,12,ctx);
+      case 11: if (this.value === 11) CardLetter.draw({x: x, y: y+h*.17},scale*3.5,this.type,11,ctx);
         CardSymbol.draw({x: x-w*.35, y: y-h*.4},scale*.6,0,this.type,ctx);
         CardSymbol.draw({x: x+w*.35, y: y+h*.4},scale*.6,0,this.type,ctx);
         break;
@@ -343,39 +454,72 @@ class Card {
 
     ctx.restore();
   }
+
+  static drawBorder(x,y,rotation,ctx) {
+    let w = ctx.canvas.height * .085,
+    h = 1.3*w,
+    r = .06*w,
+    strokeWidth = w * .05;
+
+    ctx.save();
+
+    ctx.translate(x, y);
+    ctx.rotate(rotation * Math.PI / 180);
+    ctx.translate(-x, -y);
+
+    ctx.strokeStyle = 'rgba(0,0,0,0.3)';
+    ctx.lineWidth = strokeWidth;
+    ctx.beginPath();
+    ctx.arc((x-.5*w)+r,y-.5*h,r,Math.PI,Math.PI*1.5);
+    ctx.arc((x+.5*w)-r,(y-.5*h),r,Math.PI*1.5,0);
+    ctx.arc((x+.5*w)-r,(y+.5*h),r,0,Math.PI*.5);
+    ctx.arc((x-.5*w)+r,(y+.5*h),r,Math.PI*.5,Math.PI);
+    ctx.closePath();
+    ctx.stroke();
+
+    ctx.restore();
+
+  }
 }
 
 class Table {
 
   constructor(ctx) {
     this.ctx = ctx;
+    this.cH;
+    this.cW;
+    this.h;
   }
 
   draw() {
+    this.cH = this.ctx.canvas.height;
+    this.cW = this.ctx.canvas.width;
+    this.h = Math.floor(.8*this.cH);
     this.table();
     this.tableBorder();
+    this.cardBorders();
   }
 
   table() {
     let ctx = this.ctx,
-    cW = ctx.canvas.width,
-    cH = ctx.canvas.height,
-    h = Math.floor(.6*cH),
-    w = h,
+    cW = this.cW,
+    cH = this.cH,
+    h = this.h,
+    grd,
     x = cW*.5,
     y = cH*.5;
 
     ctx.save();
-    let grd = ctx.createRadialGradient(x, y, 1, x, y, w);
+    grd = ctx.createRadialGradient(x, y, 1, x, y, h);
     grd.addColorStop(0, Color.lightGreen);
     grd.addColorStop(1, Color.darkerGreen);
     ctx.fillStyle = grd;
     ctx.beginPath();
-    ctx.moveTo(cW*.5-.5*w,cH*.5-.5*h);
-    ctx.lineTo(cW*.5+.5*w,cH*.5-.5*h);
-    ctx.arc(cW*.5+.5*w,cH*.5,h*.5,Math.PI*1.5,Math.PI*.5);
-    ctx.lineTo(cW*.5-.5*w,cH*.5+.5*h);
-    ctx.arc(cW*.5-.5*w,cH*.5,h*.5,Math.PI*.5,Math.PI*1.5);
+    ctx.moveTo(cW*.5-.5*h,cH*.5-.5*h);
+    ctx.lineTo(cW*.5+.5*h,cH*.5-.5*h);
+    ctx.arc(cW*.5+.5*h,cH*.5,h*.5,Math.PI*1.5,Math.PI*.5);
+    ctx.lineTo(cW*.5-.5*h,cH*.5+.5*h);
+    ctx.arc(cW*.5-.5*h,cH*.5,h*.5,Math.PI*.5,Math.PI*1.5);
     ctx.closePath();
     ctx.transform(1,0,0,0.7,0,h*.25);
     ctx.fill();
@@ -384,37 +528,58 @@ class Table {
 
   tableBorder() {
     let ctx = this.ctx,
-    cW = ctx.canvas.width,
-    cH = ctx.canvas.height,
-    h = Math.floor(.6*cH),
-    w = h;
+    cW = this.cW,
+    cH = this.cH,
+    h = this.h;
     ctx.strokeStyle = Color.brown;
     ctx.lineWidth = h*.1;
     ctx.beginPath();
-    ctx.moveTo(cW*.5-.5*w,cH*.5-.5*h);
-    ctx.lineTo(cW*.5+.5*w,cH*.5-.5*h);
-    ctx.arc(cW*.5+.5*w,cH*.5,h*.5,Math.PI*1.5,Math.PI*.5);
-    ctx.lineTo(cW*.5-.5*w,cH*.5+.5*h);
-    ctx.arc(cW*.5-.5*w,cH*.5,h*.5,Math.PI*.5,Math.PI*1.5);
+    ctx.moveTo(cW*.5-.5*h,cH*.5-.5*h);
+    ctx.lineTo(cW*.5+.5*h,cH*.5-.5*h);
+    ctx.arc(cW*.5+.5*h,cH*.5,h*.5,Math.PI*1.5,Math.PI*.5);
+    ctx.lineTo(cW*.5-.5*h,cH*.5+.5*h);
+    ctx.arc(cW*.5-.5*h,cH*.5,h*.5,Math.PI*.5,Math.PI*1.5);
     ctx.stroke();
     ctx.strokeStyle = Color.brown2;
     ctx.lineWidth = h*.02;
     ctx.beginPath();
-    ctx.moveTo(cW*.5-.5*w,cH*.5-.5*h);
-    ctx.lineTo(cW*.5+.5*w,cH*.5-.5*h);
-    ctx.arc(cW*.5+.5*w,cH*.5,h*.5,Math.PI*1.5,Math.PI*.5);
-    ctx.lineTo(cW*.5-.5*w,cH*.5+.5*h);
-    ctx.arc(cW*.5-.5*w,cH*.5,h*.5,Math.PI*.5,Math.PI*1.5);
+    ctx.moveTo(cW*.5-.5*h,cH*.5-.5*h);
+    ctx.lineTo(cW*.5+.5*h,cH*.5-.5*h);
+    ctx.arc(cW*.5+.5*h,cH*.5,h*.5,Math.PI*1.5,Math.PI*.5);
+    ctx.lineTo(cW*.5-.5*h,cH*.5+.5*h);
+    ctx.arc(cW*.5-.5*h,cH*.5,h*.5,Math.PI*.5,Math.PI*1.5);
     ctx.stroke();
+  }
+
+  cardBorders() {
+    let ctx = this.ctx
+
+    for(let i = .35; i < .7; i+=.05) {
+        Card.drawBorder(ctx.canvas.width * i,ctx.canvas.height * .5,0,ctx);
+    }
+
+    CardLetter.drawLetter(ctx.canvas.width * .6,
+      ctx.canvas.height * .517,
+      ctx.canvas.height * .05,
+      "S",
+      "rgba(0,0,0,0.3)",
+      ctx);
+    CardLetter.drawLetter(ctx.canvas.width * .65,
+      ctx.canvas.height * .517,
+      ctx.canvas.height * .05,
+      "D",
+      "rgba(0,0,0,0.3)",
+      ctx);
   }
 }
 
-class Chip {
-  constructor(x,y,radius, rotation,value,color,ctx) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.rotation = rotation;
+class Chip extends MovingObject {
+  constructor(x,y, rotation,value,color,ctx) {
+    super(x,y,rotation);
+    //this.x = x;
+    //this.y = y;
+    //this.radius = radius;
+    //this.rotation = rotation;
     this.value = value;
     this.color = color;
     this.ctx = ctx;
@@ -424,8 +589,8 @@ class Chip {
     let ctx = this.ctx,
     x = this.x,
     y = this.y,
-    radius = this.radius,
-    rot = this.rotation; //to be controlled via scale (maybe put scale up in the table class)
+    radius = ctx.canvas.height * .015,
+    rot = this.rotation;
 
     ctx.save();
     ctx.fillStyle = this.color;
@@ -480,6 +645,15 @@ class CardLetter {
     ctx.fillText(text, x, y);
     ctx.restore();
 
+  }
+
+  static drawLetter(x,y,size,text,color,ctx) {
+    ctx.save();
+    ctx.fillStyle = color;
+    ctx.font = size + 'px sans-serif';
+    ctx.textAlign = "center";
+    ctx.fillText(text, x, y);
+    ctx.restore();
   }
 }
 
@@ -593,4 +767,15 @@ class CardSymbol {
 }
 
 let f = new PokerGameGui();
-f.reset();
+f.addCard(CardType.PIKES,CardValue.KING);
+f.addChip(20);
+//f.addCard(CardType.HEARTS,CardValue.KING);
+// f.renderIngame();
+// f.updateGO();
+// f.renderIngame();
+f.start();
+
+// setInterval(() => {
+//   f.renderIngame();
+//   f.updateGO();
+// },1000);
